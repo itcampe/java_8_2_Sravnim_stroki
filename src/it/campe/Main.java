cd9747f28517e02f104b83b141aab18530dbb787
Ksenx73 <hello@itcampe.com>
2021-10-19 12:10:48 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,11 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("строки одинаковые");
+        //напишите тут ваш код
+        System.out.println("строки разные");
+    }
+}
